190b0bf70c6d104ee94179cfdf9f367075b99073
Patricia <patriciamvbog@gmail.com>
2023-07-04 12:51:08 +0100

    add function for us to plot the gorings on the y-axis

--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -9,9 +9,9 @@
                 stroke="#c4c4c4"
                 stroke-width="2"
                 x1="0"
-                y1="100"
+                :y1="zero"
                 x2="300"
-                y2="100"
+                :y2="zero"
             />
             <polyline
                 fill="none"
@@ -29,7 +29,7 @@
             />
         </svg>
         <p>The last 30 days</p>
-      <div>{{ points }}</div>
+      <div>{{ zero }}</div>
     </div>
  </template>
 <!--we use the composition sintaxis-->
@@ -45,15 +45,23 @@ import { toRefs, defineProps, computed } from 'vue';
 
 const { amounts } = toRefs(props);
 
-const amountToPixels = () => {
+const amountToPixels = (amount) => {
     const min = Math.min(...amounts.value);
     const max = Math.max(...amounts.value);
-    return `${min}, ${max}`;
+
+//Math.abs Gets the absolute value of a number.
+    const amountAbs = amount + Math.abs(min);
+    const minmax = Math.abs(max) + Math.abs(min);
+
+    return 200 - ((amountAbs * 100) / minmax) * 2;
 }
+ const zero = computed(() => {
+    return amountToPixels(0);
+ });
 
  const points = computed(() => {
      const total = amounts.value.length;
-     return Array(total).fill(100).reduce((points, amount, i) => {
+     return amounts.value.reduce((points, amount, i) => {
          const x = (300 / total) * (i + 1);
          const y = amountToPixels(amount);
          console.log(y);
